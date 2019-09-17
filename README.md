66b3b8c6f0e249d66caaf757831736d5a1b24e16
Hemanth Sai <hemanthsai10@gmail.com>
2019-09-16 21:52:33 -0400

    Uploading new changes
    Intended files using Sublime intendation
    changed functions to anonymous lexical scope
    change variables to a member of a var to have only one global variable
    Updated readme

--- README.md
+++ README.md
@@ -12,6 +12,21 @@ The webpage is available at hw02.cs5610f19.website
 
 The application supports decimal point operations and operations on negative numbers.
 
-Design decision was made not to support consecutive arithmetic operations so as to have the functionality of += as intended and rest operations are only used to perform operations on the latest available numbers unless += button is entered to store the state/result. (Example : To compute 2-3-5 which would be -6, the user needs to be compute 2 - 3 and then see the result i.e use += and then perform - 5. If user interacts directly 2-3-5 then subtract - operator considers only last two digits and return result of -2.). This decision is made for += to have its intended behaviour.
+Consecutive operations are executed sequential order i.e 1 + 2 * 3 will be (1 + 2) * 3 i.e in the order of inputs entered.
 
 Not finite numbers like Infinity, NaN are displayed as UNDEFINED in the calculator
+
+Design decision for any arithmetic operator followed by - (minus) was made to handle as a negative number so to undo, the calculator clear must be used.
+
+Consecutive operator inputs i.e * followed by / will be overidden by the latest operator entered. (Unless - where its treated as neg)
+
+## Attributions : 
+Image of Polar Bear from wikepedia
+Namespace and Anonymous functions understanding 
+- https://www.codeproject.com/Articles/829254/JavaScript-Namespace
+- https://www.contentful.com/blog/2017/01/17/the-global-object-in-javascript/
+Window event listener after load - http://ccs.neu.edu/home/ntuck/courses/2019/09/cs5610/notes/02-browsers/page/code.js
+Selector concepts understanding - https://www.w3schools.com/cssref/css_selectors.asp
+Referenced W3 schools for button design
+- https://www.w3schools.com/css/css3_buttons.asp for 
+- https://www.w3schools.com/howto/howto_css_animate_buttons.asp
--- assets/script.js
+++ assets/script.js
@@ -1,116 +1,107 @@
-(function() {
-	// display state i.e display number in panel
-	var d_state;
-	var decimal_type;
-	//previous state
-	var p_state;
-	var operation;
-	var lastPerformed;
-	//display panel js object
-	var display;
-	var actionEnum = {
-	  NUM: 1,
-	  ADD: 2,
-	  OTHER: 3,
-	};
+(function(window) {
+	var calculatorObj = {}
 
-	function updateDisplay(operationPerformed = true) {
-		display.value = d_state
+	calculatorObj.updateDisplay = function(operationPerformed = true) {
+		calculatorObj.display.value = calculatorObj.d_state
 		if (operationPerformed) {
 			//resetting decimal type
-			decimal_type = false
-			display.value = ""
+			calculatorObj.decimal_type = false
+			calculatorObj.display.value = ""
 		}
 	}
 
-	function numClick() {
+	calculatorObj.numClick = function() {
 		var presVal = this.value
-		if (lastPerformed == actionEnum.ADD) {
-			p_state = d_state
-			d_state = 0
+		if (calculatorObj.lastPerformed == calculatorObj.actionEnum.ADD) {
+			calculatorObj.p_state = calculatorObj.d_state
+			calculatorObj.d_state = 0
 		}
 		if (presVal == ".") {
-			if (!decimal_type) {
-				decimal_type = true
-				d_state = String(d_state)+presVal
+			if (!calculatorObj.decimal_type) {
+				calculatorObj.decimal_type = true
+				calculatorObj.d_state = String(calculatorObj.d_state)+presVal
 			}
 		}
-		else if (d_state != 0 || decimal_type) {
-			d_state = String(d_state)+presVal
+		else if (calculatorObj.d_state != 0 || calculatorObj.decimal_type) {
+			calculatorObj.d_state = String(calculatorObj.d_state)+presVal
 		}
 		else{
-			d_state = presVal
+			calculatorObj.d_state = presVal
 		}
-		updateDisplay(false)
-		lastPerformed = actionEnum.NUM
+		calculatorObj.updateDisplay(false)
+		calculatorObj.lastPerformed = calculatorObj.actionEnum.NUM
 	}
 
-	function clearPanel() {
-		d_state = 0
-		updateDisplay()
-		display.value = 0
+	calculatorObj.clearPanel = function() {
+		calculatorObj.d_state = 0
+		calculatorObj.updateDisplay()
+		calculatorObj.display.value = 0
 	}
 
-	function operationClicked() {
+	calculatorObj.operationClicked = function() {
 		if (this.value == "+=") {
-			if (operation == undefined) {
-				p_state = d_state
-				operation = "+"
-				d_state = 0
-				updateDisplay()
+			if (calculatorObj.operation == undefined) {
+				calculatorObj.p_state = calculatorObj.d_state
+				calculatorObj.operation = "+"
+				calculatorObj.d_state = 0
+				calculatorObj.updateDisplay()
 			}
 			else {
-				lastPerformed = actionEnum.ADD
-				d_state = eval(p_state+operation+"("+d_state+")")
-				operation = "+"
-				p_state = 0
-				if (!isFinite(d_state)) {
-					d_state = "UNDEFINED"
+				calculatorObj.lastPerformed = calculatorObj.actionEnum.ADD
+				calculatorObj.d_state = eval(calculatorObj.p_state+calculatorObj.operation+"("+calculatorObj.d_state+")")
+				calculatorObj.operation = "+"
+				calculatorObj.p_state = 0
+				if (!isFinite(calculatorObj.d_state)) {
+					calculatorObj.d_state = "UNDEFINED"
 				}
-				updateDisplay()
-				display.value = d_state
+				calculatorObj.updateDisplay()
+				calculatorObj.display.value = calculatorObj.d_state
 			}
 		}
 		else{
-			if (this.value == "-" && (lastPerformed != actionEnum.NUM || d_state == 0)) {
-				d_state = this.value
-				updateDisplay(false)
+			if (this.value == "-" && (calculatorObj.lastPerformed != calculatorObj.actionEnum.NUM || calculatorObj.d_state == 0)) {
+				calculatorObj.d_state = this.value
+				calculatorObj.updateDisplay(false)
 			}
 			else{
-				if (p_state != null && operation != "+") {
-					d_state = eval(p_state+operation+"("+d_state+")")
+				if (calculatorObj.p_state != null && calculatorObj.operation != "+") {
+					calculatorObj.d_state = eval(calculatorObj.p_state+calculatorObj.operation+"("+calculatorObj.d_state+")")
 				}
-				operation = this.value
-				if (lastPerformed != actionEnum.OTHER){
-					p_state = d_state
-					d_state = 0
-					updateDisplay()
+				calculatorObj.operation = this.value
+				if (calculatorObj.lastPerformed != calculatorObj.actionEnum.OTHER){
+					calculatorObj.p_state = calculatorObj.d_state
+					calculatorObj.d_state = 0
+					calculatorObj.updateDisplay()
 				}
 			}
-			lastPerformed = actionEnum.OTHER
+			calculatorObj.lastPerformed = calculatorObj.actionEnum.OTHER
 		}
 	}
 
 	/* Attribution : Initialize all elements after load concept from lecture notes
-	   http://ccs.neu.edu/home/ntuck/courses/2019/09/cs5610/notes/02-browsers/page/code2.js
-	 */
-	function buttonsInitialize() {
-		d_state = 0
-		decimal_type = false
-		lastClickedOperation = false
-		display = document.getElementById('display')
-		display.value = d_state
-		var numBtns  = document.querySelectorAll(".num-btn")
-		numBtns.forEach(function(numBtn) {
-			numBtn.addEventListener("click", numClick)
-		})
-		clearBtn = document.getElementById('clear-btn')
-		clearBtn.addEventListener("click", clearPanel)
-		var operationBtns  = document.querySelectorAll(".oper-btn")
-		operationBtns.forEach(function(operationBtn) {
-			operationBtn.addEventListener("click", operationClicked)
-		})
-	}
-
-	window.addEventListener("load", buttonsInitialize);
-})();
+	   http://ccs.neu.edu/home/ntuck/courses/2019/09/cs5610/notes/02-browsers/page/code.js
+	   */
+	   calculatorObj.initialize = function() {
+	   	calculatorObj.d_state = 0
+	   	calculatorObj.decimal_type = false
+	   	calculatorObj.display = document.getElementById('display')
+	   	calculatorObj.display.value = calculatorObj.d_state
+	   	calculatorObj.actionEnum = {
+	   		NUM: 1,
+	   		ADD: 2,
+	   		OTHER: 3,
+	   	};
+	   	var numBtns  = document.querySelectorAll(".num-btn")
+	   	numBtns.forEach(function(numBtn) {
+	   		numBtn.addEventListener("click",calculatorObj.numClick)
+	   	})
+	   	clearBtn = document.getElementById('clear-btn')
+	   	clearBtn.addEventListener("click", calculatorObj.clearPanel)
+	   	var operationBtns  = document.querySelectorAll(".oper-btn")
+	   	operationBtns.forEach(function(operationBtn) {
+	   		operationBtn.addEventListener("click", calculatorObj.operationClicked)
+	   	})
+	   }
+	   window.calculatorObj = calculatorObj
+	})(window)
+	window.addEventListener("load", window.calculatorObj.initialize)
